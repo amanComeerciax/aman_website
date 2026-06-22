dcd3eb3b79d4e6c808de92f8ed4ca76fbde65d6c
Mo_Amaan <mohammad.aman@commerciax.com>
2026-06-22 16:33:03 +0530

    feat: add sitemap for SEO

---
+++ src/app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://www.mohammadaman.in',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+  ]
+}
